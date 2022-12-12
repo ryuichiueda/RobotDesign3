9a93b7a58c763b6f92744b626048f4585509f089
bluewhitep <20318684+bluewhitep@users.noreply.github.com>
2022-12-12 13:08:58 +0900

    fix bug

--- scripts/robot_io.py
+++ scripts/robot_io.py
@@ -53,16 +53,16 @@ class RobotIO:
 		J1_LLMT = -150
 		angles = [ a if a < J1_ULMT else J1_ULMT for a in angles ]
 		angles = [ a if a > J1_LLMT else J1_LLMT for a in angles ]
-    
+
 		s = [str(a) for a in angles ]
 		s = ['0' + a if len(a) == 1 else a for a in s ] 
 		s = ",".join(s) + '\n'
-    
+
 		try:
-			print(f"s:{s}")
 			if self.prev == s:
 				return
 			self.uart.write(s)
+			self.uart.flush()
 			self.prev = s
 			print("manipulator: {}".format(s))
 		except:
@@ -108,6 +108,7 @@ if __name__ == '__main__':
 			with open("/run/shm/ev_on_off","r") as f:
 				v = int(f.readline())
 				rio.write_ev(v)
+				
 			with open("/run/shm/ev2_on_off","r") as f:
 				v = int(f.readline())
 				rio.write_ev2(v)
